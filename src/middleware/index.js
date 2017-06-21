e8630586ce52f6da290a56412a0afe64ae4b6e02
Fernando Navarro <fnp.formacion@gmail.com>
2017-06-22 01:13:20 +0200

    req.feathers is undefined in middleware/index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -3,4 +3,11 @@ module.exports = function () {
   // in Express the order matters, `notFound` and
   // the error handler have to go last.
   const app = this; // eslint-disable-line no-unused-vars
+  
+  app.use(function(req, res, next) { // eslint-disable-line no-unused-vars
+    console.log(req.feathers);//undefined
+    req.feathers.data = 'Hello world';
+    next();
+  });
+  
 };
